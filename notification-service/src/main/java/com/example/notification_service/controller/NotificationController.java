d36851c9534cdf73589a35bdcda223ea061b8542
Hayashing only simp Kiana <thiennguyen9804@gmail.com>
2025-10-17 21:43:42 +0700

    [feat]: setup notification service

---
+++ notification-service/src/main/java/com/example/notification_service/controller/NotificationController.java
@@ -0,0 +1,12 @@
+package com.example.notification_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * NotificationController
+ */
+@RestController
+@RequestMapping("/api/notification")
+public class NotificationController {
+}
