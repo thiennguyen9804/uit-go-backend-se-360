044ad95aac6ee5d1dfd0364425ef01ed2cc30db8
Hayashing only simp Kiana <thiennguyen9804@gmail.com>
2025-11-19 15:38:43 +0700

    [feat]: make matching service's noti run on multi-thread

--- matching-service/src/main/java/com/example/matching_service/service/MatchingService.java
+++ matching-service/src/main/java/com/example/matching_service/service/MatchingService.java
@@ -14,6 +14,7 @@ import org.springframework.data.redis.connection.RedisGeoCommands.GeoLocation;
 import org.springframework.data.redis.core.GeoOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.example.matching_service.client.NotificationGrpcClient;
@@ -34,22 +35,37 @@ class MatchingService {
   private final ObjectMapper objectMapper = new ObjectMapper();
 
   @KafkaListener(topics = "trip-events")
+  @Async("threadPoolTaskExecutor")
   public void listenForTripEvent(TripEvent tripEvent) throws JsonMappingException, JsonProcessingException {
     logger.info("MatchingService received trip events");
     TripLocationData data = objectMapper.readValue(tripEvent.getData(), TripLocationData.class);
     List<Long> driverIdList = findNearbyAvailableDrivers(
         data.sourceLat(),
-        data.sourceLng(),
-        0,
+        data.sourceLng(), 
+        5,
         10);
 
+    if(driverIdList.isEmpty()) {
+      logger.info("No driver is free right now");
+    }
+
     for (Long driverId : driverIdList) {
+      logger.info("Sending notification to driver {}", driverId);
+      sendNotificationAsync(driverId, tripEvent.getTripId());
+    }
+  }
+
+  @Async
+  private void sendNotificationAsync(Long driverId, Long tripId) {
+    try {
       notificationClient.sendNotification(
           driverId,
           "New Trip Offer",
-          "Do you wanna take the trip: " + tripEvent.getTripId() + " ?");
+          "Do you wanna take the trip: " + tripId + " ?");
+      logger.info("Sent notification to driver {}", driverId);
+    } catch (Exception e) {
+      logger.error("Failed to send notification to driver {}: {}", driverId, e.getMessage());
     }
-
   }
 
   private List<Long> findNearbyAvailableDrivers(double lat, double lng, int limit, double radiusKm) {
@@ -59,7 +75,8 @@ class MatchingService {
         new Distance(radiusKm, Metrics.KILOMETERS));
 
     GeoResults<GeoLocation<String>> results = geoOps.radius("drivers:geo:free", circle);
-
+    logger.info("GeoResults's size: {}", results.getContent().size());
+    
     return results.getContent().stream()
         .map(result -> {
           String driverIdStr = result.getContent().getName();
