7756f0e0ffe53b30526872e45d8108d275dd17ee
duyan2509 <22520020@gm.uit.edu.vm>
2025-12-10 16:55:44 +0700

    add demo

---
+++ demo-service/src/main/java/com/example/demoservice/Demo ServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example.demoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo ServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Demo ServiceApplication.class, args);
+    }
+}
